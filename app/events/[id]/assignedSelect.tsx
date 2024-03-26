f55107d6d1402d6120c97f2a4121401f62a06190
Nixon <34273829+mbanda1@users.noreply.github.com>
2024-03-26 16:37:45 +0300

    Fetch data with react-query

--- app/events/[id]/assignedSelect.tsx
+++ app/events/[id]/assignedSelect.tsx
@@ -1,27 +1,28 @@
 'use client'
 import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
+import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import Skeleton from 'react-loading-skeleton'
 
 function SelectAssignee() {
-    const [users, setUsers] = useState<User[]>([])
+    const {data: users, error, isLoading} = useQuery<User[]>({
+        queryKey: ['users'],
+        queryFn: () => axios.get<User[]>('/api/users')
+                    .then((res) => res.data),
+        staleTime: 60 * 100, // 60 seconds
+        retry: 2 //retry upto 2 times if we have an error
+    })
 
-    useEffect(() => {
-        const fetch = async () => {
-            const {data} = await axios.get<User[]>('/api/users')
-            setUsers(data)
-        }
-        fetch()
-    }, [])
+    if (isLoading) return <Skeleton/>
 
     return (
         <div>
-            <Select.Root defaultValue="apple">
+            <Select.Root>
                 <Select.Trigger />
                 <Select.Content>
                     {
-                        users.map(user => {
+                        users?.map(user => {
                             return <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
                         })
                     }
