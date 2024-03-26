6a24a0e013dc014024653503f7a78eaad5ba0c26
Nixon <34273829+mbanda1@users.noreply.github.com>
2024-03-26 15:29:51 +0300

    Refactor nav-bar

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Skeleton } from '@/app/components';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -10,48 +11,71 @@ const NavBar = () => {
   const pathname = usePathname()
   const { status, data: session } = useSession()
 
+  const AuthStatus = () => {
+    if (status === 'loading') return <Skeleton width={'3rem'} />
+    if (status === 'authenticated') {
+      return <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session.user?.image!}
+            fallback='?' size={'2'}
+            radius='full'
+            className='cursor-pointer'
+            referrerPolicy='no-referrer' />
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={'2'}>
+              {session.user?.name}
+            </Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={'api/auth/signout'}>Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    }
+  }
+
   return (
     <div>
       <nav className='border-b mb-5 px-5 py-3'>
         <Container>
-        <Flex justify={'between'}>
-          <Flex align={'center'} gap={'3'}>
-          <Link href='/'> <FaClock /> </Link>
-            <ul className='flex space-x-6'>
-              <li>
-                <Link href={'/'}
-                  className={classNames({
-                    'text-zinc-400': pathname !== '/',
-                    'text-zinc-800': pathname === '/',
-                    'hover:text-zinc-700 transition-colors': true
-                  })}
-                >Dashboard</Link>
+          <Flex justify={'between'}>
+            <Flex align={'center'} gap={'3'}>
+              <Link href='/'> <FaClock /> </Link>
+              <ul className='flex space-x-6'>
+                <li>
+                  <Link href={'/'}
+                    className={classNames({
+                      'text-zinc-400': pathname !== '/',
+                      'text-zinc-800': pathname === '/',
+                      'hover:text-zinc-700 transition-colors': true
+                    })}
+                  >Dashboard</Link>
 
-                <Link href={'/events'}
-                  className={classNames({
-                    'text-zinc-400': pathname !== '/events',
-                    'text-zinc-800': pathname === '/events',
-                    'hover:text-zinc-700 transition-colors': true,
-                    'pl-3': true,
-                  })}
-                >Events</Link>
-              </li>
-            </ul>
+                  <Link href={'/events'}
+                    className={classNames({
+                      'text-zinc-400': pathname !== '/events',
+                      'text-zinc-800': pathname === '/events',
+                      'hover:text-zinc-700 transition-colors': true,
+                      'pl-3': true,
+                    })}
+                  >Events</Link>
+                </li>
+              </ul>
+            </Flex>
+            <Box>
+              {AuthStatus()}
+              {
+                status === 'unauthenticated' && (
+                  <Link href={'api/auth/signin'}>Login</Link>
+                )
+              }
+            </Box>
           </Flex>
-          <Box>
-            {
-              status === 'authenticated' && (
-                <Link href={'api/auth/signout'}>Log out</Link>
-              )
-            }
-            {
-              status === 'unauthenticated' && (
-                <Link href={'api/auth/signin'}>Login</Link>
-              )
-            }
-          </Box>
-        </Flex>
-      </Container>
+        </Container>
       </nav>
     </div>
   )
